e7c9c1949e1ba4e765cecc471930e59737c25c86
Tismeet Singh <tismeet14sh@gmail.com>
2022-09-23 13:50:38 +0530

    vocab setup

---
+++ Generator.py
@@ -0,0 +1,8 @@
+import numpy as np
+from Dataset import *
+from Vocabulary import *
+from Config import *
+
+class Generator:
+    @staticmethod
+    def data_generator(voc, gui)
---
+++ Vocabulary.py
@@ -0,0 +1,72 @@
+import sys
+import numpy as np
+
+START_TOKEN = "<START>"
+END_TOKEN = "<END>"
+PLACEHOLDER = " "
+SEPARATOR = "->"
+
+class Vocabulary:
+    def __init__(self):
+        self.binary_vocabulary = {}
+        self.vocabulary = {}
+        self.token_lookup = {}
+        self.size = 0
+
+        self.addToken(START_TOKEN)
+        self.addToken(END_TOKEN)
+        self.addToken(PLACEHOLDER)
+
+
+    def addToken(self, token):
+        if token not in self.vocabulary:
+            self.vocabulary[token] = self.size
+            self.token_lookup[self.size] = token
+            self.size += 1
+
+    def binaryConvert(self):
+        items = self.vocabulary.items()
+        for key, value in items:
+            binary = np.zeros(self.size)
+            binary[value] = 1
+            self.binary_vocabulary[key] = binary
+    
+    def serializeBinaryConvert(self):
+        if len(self.binary_vocabulary) == 0:
+            self.binaryConvert()
+        
+        string = ""
+        items = self.binary_vocabulary.items()
+        for key, value in items:
+            strarr = np.array2string(value, separator=',', max_line_width = self.size*self.size)
+            str += "{}{}{}\n".format(key, SEPARATOR, strarr[1:len(strarr) - 1])
+    
+        return str
+
+    def save(self, path):
+        output_file = "{}/words.vocab".format(path)
+        file = open(output_file, 'w')
+        file.write(self.serializeBinaryConvert())
+        file.close()
+    
+    def dataRead(self, path):
+        input_file = "{}/words.vocab".format(path)
+        file = open(input_file, 'r')
+        buffer = ""
+        for line in input_file:
+            try:
+                separator_position = len(buffer) + line.index(SEPARATOR)
+                buffer += line
+                key = buffer[:separator_position]
+                value = buffer[separator_position + len(SEPARATOR):]
+                value = np.fromstring(value, sep = ',')
+
+                self.binary_vocabulary[key] = value
+                self.vocabulary[key] = np.where(value == 1)[0][0]
+                self.token_lookup[np.where(value == 1)[0][0]] = key
+
+                buffer = ""
+            except ValueError:
+                buffer += line
+        file.close()
+        self.size= len(self.vocabulary)
